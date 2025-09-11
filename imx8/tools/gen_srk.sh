b6259cb6fc60a2b864e95651c03b0a06200ea0f6
Valentin Raevsky <valentin@compulab.co.il>
2025-09-11 11:22:26 +0300

    tools: gen_srk: Add WORKDIR handling
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>

--- imx8/tools/gen_srk.sh
+++ imx8/tools/gen_srk.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+#!/bin/bash -xe
+
+WORKDIR=$(dirname $(readlink -e ${BASH_SOURCE[0]}))
 
 SRKTOOL=$(which srktool)
-SRKTOOL=${SRKTOOL:-"../linux64/bin/srktool"}
+SRKTOOL=${SRKTOOL:-"${WORKDIR}/../linux64/bin/srktool"}
 
-$(pwd)/${SRKTOOL} -h 4 -t SRK_1_2_3_4_table.bin -e SRK_1_2_3_4_fuse.bin -d sha256 -c SRK1_sha256_2048_65537_v3_ca_crt.pem,SRK2_sha256_2048_65537_v3_ca_crt.pem,SRK3_sha256_2048_65537_v3_ca_crt.pem,SRK4_sha256_2048_65537_v3_ca_crt.pem -f 1
+${SRKTOOL} -h 4 -t SRK_1_2_3_4_table.bin -e SRK_1_2_3_4_fuse.bin -d sha256 -c SRK1_sha256_2048_65537_v3_ca_crt.pem,SRK2_sha256_2048_65537_v3_ca_crt.pem,SRK3_sha256_2048_65537_v3_ca_crt.pem,SRK4_sha256_2048_65537_v3_ca_crt.pem -f 1
