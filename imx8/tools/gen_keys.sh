2e0fbfc908540a263b1ea09625666370c37c0a00
Valentin Raevsky <valentin@compulab.co.il>
2025-09-11 11:22:06 +0300

    tools: gen_keys: Add WORKDIR handling
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>

--- imx8/tools/gen_keys.sh
+++ imx8/tools/gen_keys.sh
@@ -1,1 +1,4 @@
-$(pwd)/../keys/hab4_pki_tree.sh -existing-ca n -kt rsa -kl 2048 -duration 10 -num-srk 4 -srk-ca y
+#!/bin/bash -xe
+#
+WORKDIR=$(dirname $(readlink -e ${BASH_SOURCE[0]}))
+${WORKDIR}/../keys/hab4_pki_tree.sh -existing-ca n -kt rsa -kl 2048 -duration 10 -num-srk 4 -srk-ca y
