df518684a89eca55e09b42107338b303241c2c17
Valentin Raevsky <valentin@compulab.co.il>
2025-09-12 13:43:07 +0300

    tools: gen_srk: Allow running after a which failure
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>

--- imx8/tools/gen_srk.sh
+++ imx8/tools/gen_srk.sh
@@ -2,7 +2,7 @@
 
 WORKDIR=$(dirname $(readlink -e ${BASH_SOURCE[0]}))
 
-SRKTOOL=$(which srktool)
+SRKTOOL=$(which srktool || true)
 SRKTOOL=${SRKTOOL:-"${WORKDIR}/../linux64/bin/srktool"}
 
 ${SRKTOOL} -h 4 -t SRK_1_2_3_4_table.bin -e SRK_1_2_3_4_fuse.bin -d sha256 -c SRK1_sha256_2048_65537_v3_ca_crt.pem,SRK2_sha256_2048_65537_v3_ca_crt.pem,SRK3_sha256_2048_65537_v3_ca_crt.pem,SRK4_sha256_2048_65537_v3_ca_crt.pem -f 1
